c35030038bdfb8a592bdd52964bab4be6a796a4f
trspeb <trspeb@gmail.com>
2018-04-10 22:53:28 +0200

    implementation of AInext, tested

--- mastermind1.py
+++ mastermind1.py
@@ -16,7 +16,7 @@ class Combination:
     keys = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
     len_keys = len(keys)
 
-    def __init__(self, comb="1234"):
+    def __init__(self, comb="0110"):
         self.comb = comb
         self.len = len(comb)
         self.iter = list(range(self.len))
@@ -54,21 +54,44 @@ class Combination:
     def __repr__(self):
         return(self.comb)
         
-    def next(self, n, c):
+    def next(self, c = 6):
         """
         Look at the "next" combination, knowing "color base" and digit number
+
+        In case no next combination exists, "0"*self.len is return
         """
         i = 0
         temp = self.comb
-        while (i < n):
+        while (i < self.len):
             j = self.keys.find(temp[i])
-            if (j < c):
+            if (j < (c-1)):
                 temp = temp[:i] + self.keys[j+1] + temp[(i+1):]
                 return(Combination(temp))
             else:
                 temp = temp[:i] + self.keys[0] + temp[(i+1):]
                 i = i + 1
-        return(Combination("1"*n))
+        return(Combination("0"*self.len))
+
+    def next2(self, c = 6):
+        """
+        Alternative implementation of next method
+
+        In case no next combination exists, "0"*self.len is return        
+        """
+        temp = ""
+        fin = True
+        for i in self.comb:
+            j = self.keys.find(i)
+            if ((j < (c-1))&fin):
+                temp += self.keys[j+1]
+                fin = False
+            elif not(fin):
+                temp += i
+            else:
+                temp += self.keys[0]
+        if fin:
+            temp = "0"*self.len
+        return(Combination(temp))
 
 class RandomCombination(Combination):
     """
@@ -79,7 +102,6 @@ class RandomCombination(Combination):
         comb = ""
         for i in range(n):
             comb += self.keys[random.randint(0,c-1)]
-#        print(comb)
         super().__init__(comb)
         
 class AIdumb:
@@ -101,7 +123,7 @@ class AIdumb:
 
 class AIdumbnext:
     """
-    Class implementing an methodic dumb player, playong the next combo
+    Class implementing an methodic dumb player, playing the next combo
     """
     
     def __init__(self, game, silent=False):
@@ -112,13 +134,13 @@ class AIdumbnext:
         i = 0
         while (notwon & (i<max)):
             if (i==0):
-                newtry = Combination("1"*self.game.n)
+                newtry = Combination("0"*self.game.n)
             else:
                 # roll, brute force ?
                 # find the next available combo !
                 # recursively ??
                 # would be more rational to build a ruleset ?
-                newtry = self.game.trace[-1][0].next(self.game.n, self.game.c)
+                newtry = self.game.trace[-1][0].next(self.game.c)
                 pass
             rep = self.game.propose(newtry)
             notwon = (self.game.trace[-1][1][0] != 4)
@@ -133,29 +155,37 @@ class AInext:
     def __init__(self, game, silent=False):
         self.game = game
         
-    def play(self, max = 1e6):
+    def play(self, start = None, max = 1e6):
+        if start == None:
+            start = "0"*self.game.n
         notwon = True
         i = 0
         while (notwon & (i<max)):
             if (i==0):
-                newtry = Combination("1"*self.game.n)
+                newtry = Combination(start)
+            elif (i>max):
+                print("max iteration reached!!!")
+                break
             else:
-                # roll, brute force ?
-                # find the next available combo !
-                # recursively ??
-                # would be more rational to build a ruleset ?
-                newtry = self.game.trace[-1][0].next(self.game.n, self.game.c)
-                pass
+                newtry = Combination("0"*self.game.n)
+                while not(self.available(newtry)):
+                    newtry = newtry.next(self.game.c)
             rep = self.game.propose(newtry)
             notwon = (self.game.trace[-1][1][0] != 4)
             i = i+1
             print("{0} - {1}".format(i, rep))
+        return(i)
             
     def available(self, comb):
         """
         Tell me if this combination is available, from past master answers
         """
-        pass
+        # for a combination to be available, all past answers have to match
+        g = Game(comb, self.game.c, self.game.duplicates) # if the solution were *comb*
+        ret = True
+        for d in self.game.trace:
+            ret &= ((g.root == d[0])==d[1])
+        return(ret)
 
 class Game:
     """
@@ -173,3 +203,17 @@ class Game:
         self.trace.append((combination, self.root == combination))
         return(self.trace[-1])
     
+def testAll():
+    """
+    This function tries all starting combinations
+    """
+    c = Combination("0000")
+    results = []
+    first = True
+    while ((c.comb != "0000") | first):
+        first = False
+        g = Game(c)
+        ai = AInext(g)
+        results.append(ai.play("1234"))
+        c = c.next(4)
+    return(results)
